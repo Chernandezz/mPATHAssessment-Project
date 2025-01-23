d49ce6db7cb55538b066a456ce7d627e37f88b5a
Cristian Hernandez <crissstianfhz@gmail.com>
2025-01-23 09:24:48 -0500

    Updated DbConnection with new configurations

---
+++ Model/Context/DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    public partial class DbConnection : DbContext
+    {
+        private DbConnection(string connectionString)
+            : base(connectionString)
+        { 
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+            this.Database.CommandTimeout = 900;
+        }
+
+        public static DbConnection Create()
+        {
+            return new DbConnection("name=DbConnection");
+        }
+    }
+}
