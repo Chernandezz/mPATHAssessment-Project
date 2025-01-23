c46e0ed31922f463c7b25b01293348a232213cbe
Cristian Hernandez <crissstianfhz@gmail.com>
2025-01-23 14:22:46 -0500

    Data access layer added

---
+++ Data/DAL/DoctorDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.ViewModels;
+
+namespace Data.DAL
+{
+    public class DoctorDAL
+    {
+        public static PagedListVMR<DoctorVMR> GetAll(int count, int page, string searchText) 
+        {
+            PagedListVMR<DoctorVMR> response = new PagedListVMR<DoctorVMR> ();
+            return response;
+        }
+        GetById() { }
+        Create() { }
+        Update() { }
+        Delete() { }
+    }
+}
