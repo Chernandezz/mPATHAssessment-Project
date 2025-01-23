493344ccfb71e192e678186501fc80184b30576a
Cristian Hernandez <crissstianfhz@gmail.com>
2025-01-23 14:53:55 -0500

    Data layer get methods added

--- Data/DAL/DoctorDAL.cs
+++ Data/DAL/DoctorDAL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Common.ViewModels;
+using Model.Models;
 
 namespace Data.DAL
 {
@@ -12,11 +13,28 @@ namespace Data.DAL
         public static PagedListVMR<DoctorVMR> GetAll(int count, int page, string searchText) 
         {
             PagedListVMR<DoctorVMR> response = new PagedListVMR<DoctorVMR> ();
+
+            using( var db = DbConnection.Create())
+            {
+
+            }
+
             return response;
         }
-        GetById() { }
-        Create() { }
-        Update() { }
-        Delete() { }
+        public static DoctorVMR GetById(long id) 
+        {
+            DoctorVMR item = null;
+            return item;
+
+        }
+        public static long Create(Doctor item) {
+            long id = 0;
+            return id;
+        
+        }
+        public static void Update(DoctorVMR item) {
+        
+        }
+        public static void Delete(List<long> ids) { }
     }
 }
