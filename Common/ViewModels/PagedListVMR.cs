5f7abb11de3581a5aaccfa11b94e512dd1470cf1
Cristian Hernandez <crissstianfhz@gmail.com>
2025-01-23 14:23:27 -0500

    Common classes added

---
+++ Common/ViewModels/PagedListVMR.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ViewModels
+{
+    public class PagedListVMR<T>
+    {
+        public int totalCount { get; set;}
+        public IEnumerable<T> element { get; set; }
+
+        public PagedListVMR() 
+        {
+            element = new List<T>();
+            totalCount = 0;
+        }
+    }
+}
