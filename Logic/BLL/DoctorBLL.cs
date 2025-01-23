6a5eb01d5f6bedbca0fa7665b27a21c900756197
Cristian Hernandez <crissstianfhz@gmail.com>
2025-01-23 18:11:15 -0500

    Business logic added

---
+++ Logic/BLL/DoctorBLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.ViewModels;
+using Data.DAL;
+using Model.Models;
+
+namespace Logic.BLL
+{
+    public class DoctorBLL
+    {
+        public static PagedListVMR<DoctorVMR> GetAll(int count, int page, string searchText)
+        {
+            return DoctorDAL.GetAll(count, page, searchText);
+
+        }
+        public static DoctorVMR GetById(long id)
+        {
+            return DoctorDAL.GetById(id);
+
+        }
+        public static long Create(Doctor item)
+        {
+            return DoctorDAL.Create(item);
+        }
+        public static void Update(DoctorVMR item)
+        {
+            return DoctorDAL.Update(item);
+        }
+        public static void Delete(List<long> ids)
+        {
+            return DoctorDAL.Delete(ids);
+        }
+    }
+}
