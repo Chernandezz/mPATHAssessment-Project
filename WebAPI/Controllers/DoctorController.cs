4e9cc1fd2b4cbfa653a68170a8a1d52b1afe3be8
Cristian Hernandez <crissstianfhz@gmail.com>
2025-01-23 21:18:27 -0500

    CORS Added

--- WebAPI/Controllers/DoctorController.cs
+++ WebAPI/Controllers/DoctorController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Common.ViewModels;
 using Logic.BLL;
 using Model.Models;
 
 namespace WebAPI.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class DoctorController : ApiController
     {
 
