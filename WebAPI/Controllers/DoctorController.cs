b60249d2d8679918c78a91d336bf3d304c2b2ff4
Cristian Hernandez <crissstianfhz@gmail.com>
2025-01-24 09:13:44 -0500

    CRUD Endpoints created

--- WebAPI/Controllers/DoctorController.cs
+++ WebAPI/Controllers/DoctorController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Web.UI;
 using Common.ViewModels;
 using Logic.BLL;
 using Model.Models;
@@ -36,25 +37,99 @@ namespace WebAPI.Controllers
             return Content(response.code, response);
 
         }
-        /*
 
+        [HttpGet]
         public IHttpActionResult GetById(long id)
         {
+            var response = new ResponseVMR<DoctorVMR>();
 
+            try
+            {
+                response.data = DoctorBLL.GetById(id);
+            }
+            catch (Exception e)
+            {
+                response.code = HttpStatusCode.InternalServerError;
+                response.data = null;
+                response.mesages.Add(e.Message);
+                response.mesages.Add(e.ToString());
+            }
+
+            if(response.data == null && response.mesages.Count() == 0)
+            {
+                response.code = HttpStatusCode.NotFound;
+                response.mesages.Add("Item not found");
+            }
+
+            return Content(response.code, response);
         }
 
+        [HttpPost]
         public IHttpActionResult Create(Doctor item)
         {
 
+            var response = new ResponseVMR<long?>();
+
+            try
+            {
+                response.data = DoctorBLL.Create(item);
+            }
+            catch (Exception e)
+            {
+                response.code = HttpStatusCode.InternalServerError;
+                response.data = null;
+                response.mesages.Add(e.Message);
+                response.mesages.Add(e.ToString());
+            }
+
+            return Content(response.code, response);
+
         }
+
+
+        [HttpPut]
         public IHttpActionResult Update(long id, DoctorVMR item)
         {
+            var response = new ResponseVMR<bool>();
+
+            try
+            {
+                item.id = id;
+                DoctorBLL.Update(item);
+                response.data = true;
+            }
+            catch (Exception e)
+            {
+                response.code = HttpStatusCode.InternalServerError;
+                response.data = false;
+                response.mesages.Add(e.Message);
+                response.mesages.Add(e.ToString());
+            }
+
+            return Content(response.code, response);
 
         }
+
+        [HttpDelete]
         public IHttpActionResult Delete(List<long> ids)
         {
+            var response = new ResponseVMR<bool>();
 
+            try
+            {
+                DoctorBLL.Delete(ids);
+                response.data = true;
+            }
+            catch (Exception e)
+            {
+                response.code = HttpStatusCode.InternalServerError;
+                response.data = false;
+                response.mesages.Add(e.Message);
+                response.mesages.Add(e.ToString());
+            }
+
+            return Content(response.code, response);
         }
-        */
+
     }
 }
