327824974474f9ad691305f53549930a0141e8dd
Cristian Hernandez <crissstianfhz@gmail.com>
2025-01-25 11:02:14 -0500

    Admission front

--- mPathFrontEnd/src/app/routes.module.ts
+++ mPathFrontEnd/src/app/routes.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { globalRoutes } from "./modules/global/global.routing";
 import { doctorRoutes } from "./modules/doctor/doctor.routing";
+import { admissionRoutes } from "./modules/admission/admission.routing";
 
 @NgModule({
   imports: [RouterModule.forChild([
     ...globalRoutes,
-    ...doctorRoutes
+    ...doctorRoutes,
+    ...admissionRoutes
   ])],
   exports: [ RouterModule]
 })
