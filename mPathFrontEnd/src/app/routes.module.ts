6ffd57223ff1e7ecab453b3e4fae95317f35b7e2
Cristian Hernandez <crissstianfhz@gmail.com>
2025-01-25 23:04:25 -0500

    Patient front created

--- mPathFrontEnd/src/app/routes.module.ts
+++ mPathFrontEnd/src/app/routes.module.ts
@@ -3,12 +3,14 @@ import { RouterModule } from "@angular/router";
 import { globalRoutes } from "./modules/global/global.routing";
 import { doctorRoutes } from "./modules/doctor/doctor.routing";
 import { admissionRoutes } from "./modules/admission/admission.routing";
+import { patientRoutes } from "./modules/patient/patient.routing";
 
 @NgModule({
   imports: [RouterModule.forChild([
     ...globalRoutes,
     ...doctorRoutes,
-    ...admissionRoutes
+    ...admissionRoutes,
+    ...patientRoutes
   ])],
   exports: [ RouterModule]
 })
